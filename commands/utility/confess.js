96fdce77ba58e0873ace6821a39bd5a7405a834c
Benjamin Yang <benjamin.yang12@outlook.com>
2025-12-25 20:58:30 +0800

    very basic anon confessions command :D

---
+++ commands/utility/confess.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder, TextChannel, MessageFlags } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+    .setName('confess')
+    .setDescription('confess something anonymously')
+    .addStringOption((option) => option.setName('confession').setDescription("your confession").setRequired(true)),
+  async execute(interaction) {
+	  const confession = interaction.options.getString('confession');
+    interaction.client.channels.cache.get(process.env.CONFESSIONS_CHANNEL).send(confession);
+		await interaction.reply({ content: "success", flags: MessageFlags.Ephemeral });
+  },
+};
