e87e1997f0716748a68c1a3fa3aeebcd69e8c950
Benjamin Yang <benjamin.yang12@outlook.com>
2025-12-25 21:20:34 +0800

    get confessions channel id from jank settings setup

--- commands/utility/confess.js
+++ commands/utility/confess.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder, MessageFlags } = require('discord.js');
+const fs = require('fs');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -7,7 +8,36 @@ module.exports = {
     .addStringOption((option) => option.setName('confession').setDescription("your confession").setRequired(true)),
   async execute(interaction) {
 	  const confession = interaction.options.getString('confession');
-    interaction.client.channels.cache.get(process.env.CONFESSIONS_CHANNEL).send(confession);
+
+    if (!fs.existsSync("settings.json")) {
+      await interaction.reply({
+				content: "settings.json does not exist. Please generate it by running /settings confessions_channel <ID OF CONFESSIONS CHANNEL>",
+				flags: MessageFlags.Ephemeral,
+			});
+      return;
+    }
+
+    const json = JSON.parse(fs.readFileSync("settings.json"));
+
+    if (!json[interaction.guild.id]) {
+      await interaction.reply({
+				content: "This guild does not have a bot settings entry yet. Please generate it by running /settings confessions_channel <ID OF CONFESSIONS CHANNEL>",
+				flags: MessageFlags.Ephemeral,
+			});
+      return;
+    }
+
+    const channel = json[interaction.guild.id].confessions_channel;
+
+    if (!channel) {
+      await interaction.reply({
+				content: "This guild does not have a confessions channel set yet. Please generate it by running /settings confessions_channel <ID OF CONFESSIONS CHANNEL>",
+				flags: MessageFlags.Ephemeral,
+			});
+      return;
+    }
+
+    interaction.client.channels.cache.get(channel).send(confession);
 		await interaction.reply({ content: "success", flags: MessageFlags.Ephemeral });
   },
 };
