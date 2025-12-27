327a9dcf8837761784d24ef8f36f51631e60a763
Benjamin Yang <benjamin.yang12@outlook.com>
2025-12-28 04:30:33 +0800

    more intuitive convert command

--- commands/utility/convert.js
+++ commands/utility/convert.js
@@ -1,6 +1,6 @@
 // yes i think imperial units are stupid
 
-const { SlashCommandBuilder, MessageFlags } = require('discord.js');
+const { SlashCommandBuilder, MessageFlags, Message } = require('discord.js');
 const math = require('mathjs');
 
 module.exports = {
@@ -8,13 +8,12 @@ module.exports = {
     .setName('convert')
     .setDescription('convert between units')
     .addStringOption((option) => option.setName('value').setDescription("value to convert").setRequired(true))
-    .addStringOption((option) => option.setName('from').setDescription("the unit to convert from").setRequired(true))
-    .addStringOption((option) => option.setName('to').setDescription("the unit to convert to").setRequired(true)),
+    .addStringOption((option) => option.setName('from').setDescription("the unit to convert from (CASE SENSITIVE)").setRequired(true))
+    .addStringOption((option) => option.setName('to').setDescription("the unit to convert to (CASE SENSITIVE)").setRequired(true)),
   async execute(interaction) {
 	  const value = interaction.options.getString('value');
 	  const from = interaction.options.getString('from');
 	  const to = interaction.options.getString('to');
-
 
     const units = [
       "nm", "um", "mm", "cm", "m", "km", // metric distance values
@@ -24,6 +23,21 @@ module.exports = {
       "ng", "ug", "mg", "cg", "g", "kg", "t", // metric weight values
       "gr", "dr", "oz", "lb", "cwt", "ton", // stupid volume values
     ];
+
+    if (value == "list" && from == "0" && to == "0") {
+      await interaction.reply({
+        content: `Available units: ${units.join(", ")}`,
+        flags: MessageFlags.Ephemeral
+      });
+      return;
+    }
+
+    if (!units.includes(from) || !units.includes(to)) {
+      await interaction.reply({
+        content: "The units are invalid, please note they are case sensitive, for example L, kL, etc. You can view a list of available units by passing value:list from:0 to:0.",
+        flags: MessageFlags.Ephemeral
+      });
+    }
 
     const out = math.unit(`${value} ${from}`).toNumeric(to).toString();
 
