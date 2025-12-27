43b52f28cd62ddc3cf7c27fbedd575a3606a6b67
Benjamin Yang <benjamin.yang12@outlook.com>
2025-12-27 16:06:55 +0800

    fix mute cmd

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -10,7 +10,6 @@ module.exports = {
     .addIntegerOption((option) => option.setName('hours').setDescription("the duration of mute in hours"))
     .addIntegerOption((option) => option.setName('days').setDescription("the duration of mute in days")),
 	async execute(interaction) {
-	  const action = interaction.options.getString('action');
 	  const user = interaction.options.getUser('user');
     const reason = interaction.options.getString('reason');
     const mins = interaction.options.getInteger('mins');
@@ -24,30 +23,26 @@ module.exports = {
     if (hours) duration += hours * 60 * 60 * 1000;
     if (days) duration += days * 24 * 60 * 60 * 1000;
 
-    switch (action) {
-      case "mute":
-        if (!interaction.member.permissions.has(PermissionsBitField.Flags.ModerateMembers)) {
-          await interaction.reply({content: "You do not have perms to mute users", flags: MessageFlags.Ephemeral});
-          return;
-        }
-
-        if (!member.moderatable) {
-          await interaction.reply({content: "cannot mute this member", flags: MessageFlags.Ephemeral});
-          return;
-        }
-
-        const maxDuration = 28 * 24 * 60 * 60 * 1000;
-        let timeoutDuration = duration;
-        if (perm || timeoutDuration > maxDuration) timeoutDuration = maxDuration; //max mute length 28 days
-
-        if (timeoutDuration === 0) {
-          await interaction.reply({content: "Please specify a duration for the mute", flags: MessageFlags.Ephemeral});
-          return;
-        }
-
-        await member.timeout(timeoutDuration, reason || `Muted by ${interaction.user.tag}`);
-        await interaction.reply(`User ${user.tag} was muted by ${interaction.user.tag}`);
-        break;
+    if (!interaction.member.permissions.has(PermissionsBitField.Flags.ModerateMembers)) {
+      await interaction.reply({content: "You do not have perms to mute users", flags: MessageFlags.Ephemeral});
+      return;
     }
+
+    if (!member.moderatable) {
+      await interaction.reply({content: "cannot mute this member", flags: MessageFlags.Ephemeral});
+      return;
+    }
+
+    const maxDuration = 28 * 24 * 60 * 60 * 1000;
+    let timeoutDuration = duration;
+    if (perm || timeoutDuration > maxDuration) timeoutDuration = maxDuration; //max mute length 28 days
+
+    if (timeoutDuration === 0) {
+      await interaction.reply({content: "Please specify a duration for the mute", flags: MessageFlags.Ephemeral});
+      return;
+    }
+
+    await member.timeout(timeoutDuration, reason || `Muted by ${interaction.user.tag}`);
+    await interaction.reply(`User ${user.tag} was muted by ${interaction.user.tag}`);
   }
 };
